861d462f8eb771583d736461febeb564e8c496ac
Ghezae Goitom <ggappsww@gmail.com>
2022-10-06 12:03:18 +0200

    creating cookiecutter template for our work

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project is for creating a system that can enahnce an image',
+    author='image_enhancement_team',
+    license='MIT',
+)
