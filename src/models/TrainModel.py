b731018ce661a92d9fd3205de1849f97e661cd3f
Ghezae Goitom <ggappsww@gmail.com>
2022-12-13 19:56:23 +0200

    linter github action

--- src/models/TrainModel.py
+++ src/models/TrainModel.py
@@ -14,7 +14,7 @@ orginalPath = str(Path(Path(Path(__file__).parent.absolute()).parent.absolute())
 
 class TrainModel:
 
-    def read_data(file):
+    def read_data(self,file):
         with h5py.File(file, 'r') as hf:
             data = numpy.array(hf.get('data'))
             label = numpy.array(hf.get('label'))
@@ -24,7 +24,7 @@ class TrainModel:
 
 
     # define the SRCNN model
-    def srcnnModel(layer1, layer2, layer3):
+    def srcnnModel(self,layer1, layer2, layer3):
         
         # define model type
         SRCNN = Sequential()
@@ -58,8 +58,8 @@ class TrainModel:
 
         srcnn_model = self.srcnnModel(layer1,layer2,layer3)
         print(srcnn_model.summary())
-        data, label = self.read_data(orginalPath+"/data/processed/train.h5")
-        val_data, val_label = self.read_data(orginalPath+"/data/processed/test.h5")
+        data, label = self.read_data(orginalPath+"/data/processed/training/train.h5")
+        val_data, val_label = self.read_data(orginalPath+"/data/processed/training/test.h5")
 
         checkpoint = ModelCheckpoint(f"{orginalPath}/src/weights/SRCNN_weight_{layer2}.h5", monitor='val_loss', verbose=1, save_best_only=True,
                                     save_weights_only=False, mode='min')
@@ -96,15 +96,15 @@ class TrainModel:
         return arr
 
 
-    if __name__ == "__main__":
-        params = yaml.safe_load(open(f'{orginalPath}/src/models/params.yaml'))["learn"]["kernel_size"]
-        for row in params:
-            # Start mlflow run
-            mlflow.start_run()
-            train(row[0],row[1],row[2])
-            # End mlflow run
-            mlflow.end_run()
+if __name__ == "__main__":
+    params = yaml.safe_load(open(f'{orginalPath}/src/models/params.yaml'))["learn"]["kernel_size"]
+    for row in params:
+        # Start mlflow run
+        mlflow.start_run()
+        TrainModel().train(row[0],row[1],row[2])
+        # End mlflow run
+        mlflow.end_run()
 
 
-    # dvc run -n training -d src/models/params.yaml -d data/processed/train.h5 -d data/processed/test.h5 -M src/metrics/train_metric.txt -o src/weights/SRCNN_weight_1.h5 -o src/weights/SRCNN_weight_3.h5 -o src/weights/SRCNN_weight_5.h5 python src/models/train_model.py
+# dvc run -n training -d src/models/params.yaml -d data/processed/train.h5 -d data/processed/test.h5 -M src/metrics/train_metric.txt -o src/weights/SRCNN_weight_1.h5 -o src/weights/SRCNN_weight_3.h5 -o src/weights/SRCNN_weight_5.h5 python src/models/train_model.py
 
